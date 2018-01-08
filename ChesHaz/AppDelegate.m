d90556638a80481340a51cc4d4a0703b869b95b7
ches <ches@cheswick.com>
2018-01-08 15:53:07 -0500

    ready for update?

--- ChesHaz/AppDelegate.m
+++ ChesHaz/AppDelegate.m
@@ -54,7 +54,7 @@ didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    [VC layoutViews];
+//    [VC layoutViews];
 }
 
 
